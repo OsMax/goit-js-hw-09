3ceef7ee8965d51ff4c30f50200b3bcc9abcce78
OsMax <www.osmax@gmail.com>
2023-06-04 17:22:18 +0300

    first task ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+btnStop.disabled = true;
+let intervalId = null;
+
+btnStart.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+});
+btnStop.addEventListener('click', () => {
+  clearInterval(intervalId);
+  btnStart.disabled = false;
+  btnStop.disabled = true;
+});
